0f3b464e043f0066d6039d2ff9627ce8e60569be
Mai Diep <maidiep.ptit@gmail.com>
2018-02-17 21:44:30 +0700

    add class decorator.py

---
+++ class_decorator.py
@@ -0,0 +1,17 @@
+
+def my_class_decorator(cls):
+  for name, attr in vars(cls).items():
+    print(name)
+  return cls
+
+
+class Temparature():
+  
+  def __init__(self, kelvin):
+    self._kelvin = kelvin
+
+  def get_kelvin(self):
+    return self._kelvin
+
+  def set_kelvin(self, value):
+    self._kelvin = value
